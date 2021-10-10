c62a56a9b6a8cd30ef1810fbaa73eafb42826942
mitya-storozhuk <88045766+mitya-storozhuk@users.noreply.github.com>
2021-10-10 21:34:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,41 @@ let worker = {
 
 function showProps(obj) {
     let keys = Object.keys(obj);
-    let values = Object.values(obj)
+    let values = Object.values(obj);
     console.log(keys);  
     console.log(values); 
 };
 
 showProps(worker);
+
+// task3
+const currentYear = new Date();
+class Person {
+    constructor (name, surname) {
+        this.name = name;
+        this.surname = surname;
+    };
+    showFullName() {
+        console.log(this.name + " " + this.surname);
+    };
+};
+
+class Student extends Person {
+    constructor (name, surname, year) {
+        super (name, surname);
+        this.year = year;
+    };
+    
+    showFullName(midleName) {
+        this.midleName = midleName;
+        console.log(this.name + " " + this.surname + " " + this.midleName);
+    };
+
+    showCourse() {
+          return currentYear.getFullYear() - this.year;
+    }
+};
+ 
+const stud1 = new Student("Petro", "Petrenko", 2015);
+console.log(stud1.showFullName("Petrovych"));
+console.log("Current course: " + stud1.showCourse());
