c376ea934c7a9d5f03c97ee805491f539de2410c
mitya-storozhuk <88045766+mitya-storozhuk@users.noreply.github.com>
2021-10-10 20:56:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+// task1
 let mentor = { 
-            course: "JS fundamental", 
-            duration: 3,
-            direction: "web-development",
-            dfgsfdg: 5
-        };
+    course: "JS fundamental", 
+    duration: 3,
+    direction: "web-development"
+};
+
 function propsCount(mentor) {
     return console.log(Object.keys(mentor).length);
 };
 propsCount(mentor);
+
+// task2
+let worker = {
+    firstName: "Ivan",
+    lastName: "Petrov",
+    age: 38,
+    isMarried: true,
+    car: "Dodge"
+};
+
+function showProps(obj) {
+    let keys = Object.keys(obj);
+    let values = Object.values(obj)
+    console.log(keys);  
+    console.log(values); 
+};
+
+showProps(worker);
