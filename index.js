7cf61b2a2fedc278b4896d632a2c7e461c7e07cd
mitya-storozhuk <88045766+mitya-storozhuk@users.noreply.github.com>
2021-10-10 19:08:04 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+let mentor = { 
+            course: "JS fundamental", 
+            duration: 3,
+            direction: "web-development",
+            dfgsfdg: 5
+        };
+function propsCount(mentor) {
+    return console.log(Object.keys(mentor).length);
+};
+propsCount(mentor);
